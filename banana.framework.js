a8b05c7e40d9c995ffa0be005eefebf81601a1d8
Apaxaa Studios <91748627+phaxadstudios@users.noreply.github.com>
2021-12-25 22:27:06 +0100

    Add files via upload

---
+++ banana.framework.js
@@ -0,0 +1,50 @@
+/**** Banana Ols Library script 
+copyrights of Apaxaa Studios Inc 
+
+rename it to .ols when you are working, it was named .js so you could see how it works
+
+Future License www.apaxaa.com/scripts/Olsen/path_cleaner/license
+
+version - 0.0.1
+
+reviewed : 22th November 2021
+error_includes / "total: mainstream stream"
+
+note from :Augustine ~ Hi there this is not part of the script and was injected 
+via downloading request from wherever you downloaded this script from , for now
+ we have many downloading point and we don't have a website of our own yet, due 
+ to come issues we need to review,this note is from Augustine, the happy friend 
+ of Israel, the ceo of Apaxaa , you can check out our games in our paxaa games 
+ repo down below..
+
+note from :Israel ~ Hi thanks for using our scripts, 
+path-cleaner ols is a vital must if you trying to clean your file extensions
+and it's a package which you should include in the main component. 
+it is a cleaning based package that cleans the whole application through saucy 
+pocket drap ...Please do not edit this script, to pass your particular data into
+ this script download the required pockets libraries and use pocket to make
+  your data requests
+
+**/
+
+import path;
+import OsPathDirEngineStream as OsPathDir;
+import ScriptTxxLine as script_type;
+import data_catalog as data;
+
+
+var include = void path_cleaner(private=> 'ols::debris');
+
+if (OsPathDir.run())
+   include.passed(data.url("{$http_method}://{$websitename}.{$extension}/"));
+   var fullLength = include.grab(data.url("/{$requested_page}.{$requested_page_extension}");
+   if fullLength.data.position[1] == ""
+   	break;
+   else 
+   	include.remove(.data.position[1]);
+else
+   break;
+
+/*** this script is a beta version, even if as it's first .
+please report issues to @phaxad_developers under Apaxxa studios
+remember to donate whatever you wish too **/
