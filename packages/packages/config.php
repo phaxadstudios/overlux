e221ee3df8995797623321165f41464bf96a226d
Apaxaa Studios <91748627+phaxadstudios@users.noreply.github.com>
2021-12-25 22:59:19 +0100

    Update config.php

--- packages/packages/config.php
+++ packages/packages/config.php
@@ -1,14 +1,52 @@
-<?php 
+<?php
+/*
+    Encryption class
+*/
+class Encryption{
 
-ob_start();
-session_start();
+    private $key;
+    private $cipher;
 
-$con = mysqli_connect("localhost", "root","bloomonlineshop","rusley");
+    function __construct() {
+        $env = new Env();
+        $this->key = $env->APP_ENC_KEY;
+    }
 
-if (mysqli_connect_errno()){
-	echo "failed to connect".mysqli_connect_errno();
-}
-else{
-	echo "connected";
+    function encrypt($text) {
+        $length   = 8;
+        $cstrong  = true;
+        $cipher   = 'aes-128-cbc';
+    
+      if (in_array($cipher, openssl_get_cipher_methods())){
+        $ivlen = openssl_cipher_iv_length($cipher);
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt(
+          $text, $cipher, $this->key, $options=OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary=true);
+        $encodedText = base64_encode( $iv.$hmac.$ciphertext_raw );
+      }
+    
+      return $encodedText;
+
+    }
+
+    function decrypt($hash) {
+        $c = base64_decode($hash);
+        $cipher   = 'aes-128-cbc';
+    
+      if (in_array($cipher, openssl_get_cipher_methods())){
+        $ivlen = openssl_cipher_iv_length($cipher);
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len=32);
+        $ivlenSha2len = $ivlen+$sha2len;
+        $ciphertext_raw = substr($c, $ivlen+$sha2len);
+        $plainText = openssl_decrypt(
+          $ciphertext_raw, $cipher, $this->key, $options=OPENSSL_RAW_DATA, $iv);
+    }
+    
+      return $plainText;
+
+    }
+
 }
 ?>
